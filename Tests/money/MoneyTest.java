2d9d1e4c6fd8dced48eba8bb43976c4c5f5953a1
masayuki.hirata <masayuki.h0729@gmail.com>
2018-10-29 21:26:18 +0900

    page7 初コミット．最初の一歩のテストを通るようにした

---
+++ Tests/money/MoneyTest.java
@@ -0,0 +1,16 @@
+package money;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MoneyTest {
+
+    @Test
+    public void testMulitplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
+}
---
+++ src/money/Dollar.java
@@ -0,0 +1,8 @@
+package money;
+
+public class Dollar {
+    int amount = 10;
+    Dollar(int money){}
+
+    void times(int multiplier){}
+}
