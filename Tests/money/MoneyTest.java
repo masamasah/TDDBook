36cc5635efc6379a3ddde81dbd04c9a2696ca536
masayuki.hirata <masayuki.h0729@gmail.com>
2018-11-11 20:45:51 +0900

    page.26
    意図をより語るようなテストに修正した

--- Tests/money/MoneyTest.java
+++ Tests/money/MoneyTest.java
@@ -11,11 +11,8 @@ public class MoneyTest {
     @Test
     public void testMulitplication(){
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        five.times(2);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
