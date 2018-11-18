156d1d5168c13784120a2b98f54fbfc7f57dba5a
masayuki.hirata <masayuki.h0729@gmail.com>
2018-11-18 14:54:49 +0900

    Page.108
    通貨の変換に対応

--- Tests/money/MoneyTest.java
+++ Tests/money/MoneyTest.java
@@ -59,10 +59,23 @@ public class MoneyTest {
     }
 
     @Test
-    public void testReduceMoney(){
+    public void testReduceMoney() {
         Bank bank = new Bank();
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testReduceMoneyDiffentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testIdentityRate(){
+        assertEquals(1, new Bank().rate("USD", "USD"));
     }
 
 }
